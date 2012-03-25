d8693f28e64b0ddb2ad0329c97aba5378c32f2c3
Cesar J. Alaniz <cesar@alanisoft.com>
2012-03-25 00:20:51 -0700

    Remove GET functionality. Only need PUT

--- s3-proxy.js
+++ s3-proxy.js
@@ -13,16 +13,7 @@ var s3 = knox.createClient({
 
 //var cache = config.memcache.options ? new Memcached(config.memcache.servers ,config.memcache.options) : new Memcached(config.memcache.servers);
 var server = http.createServer(function(req, res) {
-  if (req.method == 'GET') {
-    s3.get(req.url).on('response', function(res_from_s3){
-      res.writeHead(res_from_s3.statusCode,
-	{ 'content-type': res_from_s3.headers['Content-Type'] });
-      res_from_s3.on('data', function(chunk){
-        res.write(chunk);
-      });
-      res_from_s3.on('end', function(){ res.end() });
-    }).end();      
-  } else if (req.method == 'PUT') {    
+  if (req.method == 'PUT') {    
     console.log(req.headers['content-type']);
     var req_to_s3 = s3.put(req.url, {'content-length' : req.headers['content-length'], 'content-type' : req.headers['content-type']});
     req.on('data', function(chunk) {
